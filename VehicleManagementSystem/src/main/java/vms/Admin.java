d58e56a64b96e85ba0a3987cc38f7889d259cbaf
Silviu01 <i.silviu_c@yahoo.com>
2018-08-19 23:14:28 +0300

    Uploaded project with classes without code

---
+++ VehicleManagementSystem/src/main/java/vms/Admin.java
@@ -0,0 +1,10 @@
+package vms;
+
+/**
+ * Admin can get log in with valid username and password.
+ * Admin can view all registered dealer details.
+ * Admin can upload vehicle information like Vehicle name, Manufacture Year, Model Color, Cost.
+ */
+
+public class Admin {
+}
---
+++ VehicleManagementSystem/src/main/java/vms/Vehicle.java
@@ -0,0 +1,9 @@
+package vms;
+
+/**
+ *A vehicle needs to register with the application to get unique username and password.
+ *  The vehicle can get log in with valid username and password.
+ *  The vehicle can view vehicle information like Vehicle name, Manufacture Year, Model Color, Cost.
+ */
+public class Vehicle {
+}
