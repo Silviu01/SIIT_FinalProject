63d6cc91a7f3a4f0433014d5a0b0dfea520c957a
Silviu01 <i.silviu_c@yahoo.com>
2018-09-07 17:27:24 +0300

    starting directly from localhost:PORT

--- VehicleManagement/src/main/java/siit/finalProject/VehicleManagement/controller/LoginController.java
+++ VehicleManagement/src/main/java/siit/finalProject/VehicleManagement/controller/LoginController.java
@@ -20,7 +20,7 @@ public class LoginController {
     /**
      * @return
      */
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String displayLogin() {
         return "login";
     }
